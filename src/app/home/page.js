5ade54aff24d5c6a71468f3f0d86357a609bd2c2
Navaneeth B <navaneeth0041@gmail.com>
2025-06-10 21:13:11 +0530

    feat(home): add whoweare page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,11 +1,13 @@
 import Header from '@/components/Header.jsx';
 import Home from '@/components/Home.jsx';
+import WhoWeArePage from '@/components/WhoWeArePage';
 
 export default function BrandAmbassadorPage() {
   return (
     <>
       <Header />
       <Home />
+      <WhoWeArePage/>
 
     </>
   );
