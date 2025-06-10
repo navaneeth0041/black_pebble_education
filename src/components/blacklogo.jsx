062ba344ad540de0ab22f05295492ba2feb1bc13
AtharvaNair04 <atharvanair04@gmail.com>
2025-06-10 17:28:59 +0530

    Responsive vercel fix

--- src/components/blacklogo.jsx
+++ src/components/blacklogo.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const BlackLogoWithText = () => (
+const BlackLogoWithText = ({ logoHeight = 80, textHeight = 150 }) => (
   <div style={{ display: 'flex', alignItems: 'center' }}>
     <img
       src="/logo/blacklogo.png"
       alt="Black Pebble Logo"
-      style={{ height: 80 }}
+      style={{ height: logoHeight }}
     />
     <img
       src="/logo/blacktext.png"
       alt="Black Text"
-      style={{ height: 150 }}
+      style={{ height: textHeight }}
     />
   </div>
 );
