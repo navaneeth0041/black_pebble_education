ef8012038acb4c3f4a85b439be672dcc7242a92f
k0w4lzk1 <jsonborne007@gmail.com>
2025-06-14 16:05:33 +0530

    Modified: Made the redirector ,fixed directory names for routing purposes

--- src/app/value-of-money/page.js
+++ src/app/value-of-money/page.js
@@ -13,11 +13,11 @@ export default function ValueOfMoney() {
         <ValueOfMoneyHero />
         <CourseCurriculum />
         <CourseSnapshot />
-      <PreFooter
-        mainTitle="Why Learn the Value of Money So Early?"
-        subText1="Some kids grow up without the concept of pocket money.They believe money is an infinite resource.This can result in indulgence or unchecked expenses,causing financial mishaps."
-        subText2="This 2-hour session gives them a relatable and practical understanding of money's worth"
-        buttonText="Book a Counselling Session"
+        <PreFooter
+         mainTitle="Why Learn the Value of Money So Early?"
+         subText1="Some kids grow up without the concept of pocket money.They believe money is an infinite resource.This can result in indulgence or unchecked expenses,causing financial mishaps."
+         subText2="This 2-hour session gives them a relatable and practical understanding of money's worth"
+         buttonText="Book a Counselling Session"
       />
       <Footer /> 
       </>
